3df3f2bb9739039ad659260626a56c7b2db9697f
Y-Singh <47701821+Y-Singh@users.noreply.github.com>
2023-07-07 16:32:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "QSwitchControl\\README.md").read_text()
 setup(
 	name="QSwitchControl",
-	version="1.0.4",
+	version="1.0.5",
 	description="An easy-to-use and modern toggle switch for Qt Python binding PyQt",
 	long_description=README,
 	long_description_content_type="text/markdown",
-	url="https://github.com/Prx001/QSwitchControl",
+	url="https://github.com/Y-Singh/QSwitchControl",
 	author="Parsa.py",
-	author_email="munichbayern2005@gmail.com",
+	author_email="munichbayern2005@gmail.com/yuvnsingh@gmail.com",
 	license="MIT",
 	classifiers=[
 		"License :: OSI Approved :: MIT License",
@@ -23,5 +23,5 @@ setup(
 	# py_modules=["__init__", "__main__", "QSwitchControl", "QSwitchControlplugin"],
 	packages=["QSwitchControl"],
 	include_package_data=True,
-	install_requires=["PyQt5"]
+	install_requires=["PyQt6"]
 )
